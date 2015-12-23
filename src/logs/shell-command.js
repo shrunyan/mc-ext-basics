1de7741b197c859a19a494602ed0a360328a32d7
Andy Fleming <andy@andyfleming.com>
2015-12-22 19:02:54 -0800

    Refactoring for new log spec

--- src/logs/shell-command.js
+++ src/logs/shell-command.js
@@ -1,15 +1,29 @@
 'use strict'
 
-module.exports.ShellCommandLog = class ShellCommandLog {
+module.exports = {
 
-  constructor(title, stdout, stderr, exitCode) {
-    this.type = 'shell_command'
-    this.title = title
-    this.data = {
-      stdout: stdout,
-      stderr: stderr,
-      exitCode: exitCode
+  id: 'shell_command',
+  name: 'Shell Command Log',
+  description: 'Easily logs and formats output for a log made from shell execution',
+
+  generate: function(title, stdout, stderr, exitCode) {
+    return {
+      type: 'FQID',
+      title: title,
+      data:  {
+        stdout: stdout,
+        stderr: stderr,
+        exitCode: exitCode
+      }
     }
+  },
+  
+  renderDetails: function(data) {
+
+    let html = ''
+
+    return html
+
   }
 
 }
